ae47810709f046a16ee5d4e4c620d64c86419a24
Nicolás Giusepetti <nicogiusepetti888@gmail.com>
2024-08-29 19:13:14 -0300

    listo para entrega final

---
+++ src/app.js
@@ -0,0 +1,66 @@
+import express from "express"
+import productRouter from "./Routes/products.router.js"; 
+import cartRouter from "./Routes/carts.router.js";
+import viewRouter from "./Routes/views.router.js"
+import exphbs from "express-handlebars";
+import { Server } from "socket.io";
+import { ProductManager } from "./dao/db/product-manager-db.js"
+import "./database.js"
+
+const productManager = new ProductManager()
+
+
+const app = express()
+const PUERTO = 8080
+
+// Configuracion de express-handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+// Middleware
+app.use(express.json()) //Para enviar informacion en formato JSON
+app.use(express.urlencoded({ extended: true })); //Para recibir datos multiples
+app.use(express.static("./src/public"))
+
+
+//Listen
+const httpServer = app.listen(PUERTO, (req, res)=>{
+    console.log("Escuchando en el puerto 8080")
+})
+
+// Websocket
+const io = new Server(httpServer);
+
+io.on("connection", async (socket) => {
+    console.log("conectadooo");
+
+    const productos = await productManager.getProducts({limit:100});
+    socket.emit("productos", productos);
+
+    socket.on("eliminarProducto", async (id) => {
+        await productManager.deleteProduct(id);
+        
+        io.sockets.emit("productos", await productManager.getProducts({limit:100}))
+    });
+
+    socket.on("agregarProducto", async (producto) => {
+        await productManager.addProduct(producto);
+        io.sockets.emit("productos", await productManager.getProducts({limit:100}))
+    });
+
+})
+
+
+
+//Rutas
+app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
+app.use("/", viewRouter);
+
+
+
+
+
+
+
